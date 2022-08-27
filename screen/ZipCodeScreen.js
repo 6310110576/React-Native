175fc4a9c74dc486e6574c8df682fedbc84bed83
6310110576 <6310110576@psu.ac.th>
2022-08-27 23:49:46 +0700

    part5Navigation & Route Parameter

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, View, Text } from "react-native";
+import { FlatList, View, Text, StyleSheet, TouchableHighlight } from "react-native";
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
@@ -9,21 +9,34 @@ const availableZipItems = [
     { place: 'Chonburi', code: '20000' },
    ]
 
-const ZipItem = ({place, code}) => (
-    <View>
-        <Text>{place}</Text>
-        <Text>{code}</Text>
-    </View>
+const ZipItem = ({place, code, navigation}) => (
+    <TouchableHighlight onPress={() => {
+        navigation.navigate('Weather', {zipCode: code})
+    } }>
+        <View style={styles.zipItem}>
+            <Text>{place}</Text>
+            <Text>{code}</Text>
+        </View>
+    </TouchableHighlight>
 )
 
 export default function ZipCodeScreen(){
+    const navigatoin = useNavigatio()
     return (
         <FlatList
             data = {availableZipItems}
             keyExtractor = {items => items.code}
-            renderItem = {({item}) => <ZipItem {...item}/>}
+            renderItem = {({item}) => <ZipItem {...item} navigation={navigator}/>}
         />  
     )
    
 }
+const styles = StyleSheet.create({
+    zipItem:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+
+})
    
